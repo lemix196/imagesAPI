8fe6a86a7afcfd08368e1d43a03f2f3a265c75ce
glemiecha <g.lemiecha1@gmail.com>
2022-11-19 12:58:44 +0100

    create app imagesAPI

--- images/settings.py
+++ images/settings.py
@@ -31,6 +31,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    #User apps
+    'imagesAPI',
+    #Built-in apps
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
