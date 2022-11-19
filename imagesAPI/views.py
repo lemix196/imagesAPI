60eab864f5e37a6d32221a814263d6d281b3d509
glemiecha <g.lemiecha1@gmail.com>
2022-11-19 21:54:39 +0100

    filter image list by owner add

--- imagesAPI/views.py
+++ imagesAPI/views.py
@@ -3,10 +3,13 @@ from imagesAPI.models import Image
 from imagesAPI.serializers import ImageSerializer
 
 class ImageList(generics.ListCreateAPIView):
-    queryset = Image.objects.all()
     serializer_class = ImageSerializer
     permission_classes = [permissions.IsAuthenticated]
 
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
+
+
+    def get_queryset(self):
+        return Image.objects.all().filter(owner=self.request.user)
