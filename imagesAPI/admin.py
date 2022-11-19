e3dbea7428e721f4356953ab27ed67e68ca804ed
glemiecha <g.lemiecha1@gmail.com>
2022-11-19 16:07:02 +0100

    django.user override in django.admin panel

--- imagesAPI/admin.py
+++ imagesAPI/admin.py
@@ -1,6 +1,20 @@
-from django.contrib import admin
 from imagesAPI.models import Thumbnail, AccountTier, Image
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin, admin
+from django.contrib.auth.models import User
 
+from imagesAPI.models import ImageUser
+
+class ImageUserInLine(admin.StackedInline):
+    model = ImageUser
+    can_delete = False
+    verbose_name_plural = 'imageuser'
+
+class UserAdmin(BaseUserAdmin):
+    inlines = [ImageUserInLine]
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
 
 admin.site.register(Thumbnail)
 admin.site.register(AccountTier)
